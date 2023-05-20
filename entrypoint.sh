9af91ed17ac32cdcc3ab831d566c29018128fe41
Kenneth Clausen <kenneth@flex4b.com>
2023-05-20 10:36:34 +0200

    Fixed package.json source not found issue

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,7 +14,7 @@ git clone $2 /dest --depth=1
 
 echo "****** GET REQUIRED VARIABLES ******" >&2
 #VERSION=$(grep -oP '(?<="version": ")[^"]*' /source/package.json)
-VERSION=$(cat package.json | jq -r '.version')
+VERSION=$(cat /source/package.json | jq -r '.version')
 
 echo "UPDATE SOURCE VERSIONS"
 sed -i 's/"version": ".\..\.."/"version": "$VERSION"/g' /source/app.json
