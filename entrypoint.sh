3ecf0460370a0246087d2c06ba377df6d52fa914
Kenneth Clausen <me@kjc.dk>
2023-06-05 09:01:04 +0200

    Implemented support for changing versionCode

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,34 +10,37 @@ ssh-add /root/.ssh/id_rsa
 
 echo "****** SETUP SOURCE & DEST REPOS ******" >&2
 git clone $1 /source --depth=1
-git clone $2 /dest --depth=1
+git clone $2 /dist --depth=1
 
 echo "****** CLEANUP src ******" >&2
-rm -rf /dest/src
+rm -rf /dist/src
 
 echo "****** GET REQUIRED VARIABLES ******" >&2
-#VERSION=$(grep -oP '(?<="version": ")[^"]*' /source/package.json)
 VERSION=$(cat /source/package.json | jq -r '.version')
+VERSION_CODE=$(cat /source/package.json | jq -r '.versionCode')
 
 echo "UPDATE SOURCE VERSIONS"
 sed -i 's/"version": ".\..\.."/"version": "$VERSION"/g' /source/app.json
+sed -i 's/"version": [0-9]+/"version": $VERSION_CODE/g' /source/app.json
 
 echo "UPDATE DEST VERSIONS"
-sed -i "s/\"version\": \".\..\..\"/\"version\": \"$VERSION\"/g" /dest/app.json
-sed -i "s/\"version\": \".\..\..\"/\"version\": \"$VERSION\"/g" /dest/package.json
+sed -i "s/\"version\": \".\..\..\"/\"version\": \"$VERSION\"/g" /dist/app.json
+sed -i "s/\"version\": \".\..\..\"/\"version\": \"$VERSION\"/g" /dist/package.json
+sed -i 's/"version": [0-9]+/"version": $VERSION_CODE/g' /dist/app.json
+sed -i 's/"version": [0-9]+/"version": $VERSION_CODE/g' /dist/package.json
 
 echo "****** COPY FILES ******" >&2
 if [ -f "/source/.gitignore" ]; then
-  cp /source/.gitignore /dest/
+  cp /source/.gitignore /dist/
 fi
 
-cp -r /source/src /dest/
-cp /source/App.js /dest/
-cp /source/appSettings.template.json /dest
-cp /source/index.js /dest
+cp -r /source/src /dist/
+cp /source/App.js /dist/
+cp /source/appSettings.template.json /dist
+cp /source/index.js /dist
 
 echo "****** COMMIT & PUSH ******" >&2
-cd /dest
+cd /dist
 git add .
 git commit -am "action[repository-copy-action] $1 -> $2"
 git push
