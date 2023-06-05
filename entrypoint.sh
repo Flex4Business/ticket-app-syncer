93cf8ddf3a9bee706bf87e652f12d0e1e84a2144
Kenneth Clausen <me@kjc.dk>
2023-06-05 09:23:06 +0200

    Updated version and versionCode replacer to jq based

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,37 +10,43 @@ ssh-add /root/.ssh/id_rsa
 
 echo "****** SETUP SOURCE & DEST REPOS ******" >&2
 git clone $1 /source --depth=1
-git clone $2 /dist --depth=1
+git clone $2 /dest --depth=1
 
 echo "****** CLEANUP src ******" >&2
-rm -rf /dist/src
+rm -rf /dest/src
 
 echo "****** GET REQUIRED VARIABLES ******" >&2
 VERSION=$(cat /source/package.json | jq -r '.version')
 VERSION_CODE=$(cat /source/package.json | jq -r '.versionCode')
 
 echo "UPDATE SOURCE VERSIONS"
-sed -i 's/"version": ".\..\.."/"version": "$VERSION"/g' /source/app.json
-sed -i 's/"version": [0-9]+/"version": $VERSION_CODE/g' /source/app.json
+#sed -i 's/"version": ".\..\.."/"version": "$VERSION"/g' /source/app.json
+
+jq ".version = \"$VERSION\"" /source/app.json > /tmp/app.json && mv /tmp/app.json /source/app.json
+jq ".versionCode = \"$VERSION_CODE\"" /source/app.json > /tmp/app.json && mv /tmp/app.json /source/app.json
 
 echo "UPDATE DEST VERSIONS"
-sed -i "s/\"version\": \".\..\..\"/\"version\": \"$VERSION\"/g" /dist/app.json
-sed -i "s/\"version\": \".\..\..\"/\"version\": \"$VERSION\"/g" /dist/package.json
-sed -i 's/"version": [0-9]+/"version": $VERSION_CODE/g' /dist/app.json
-sed -i 's/"version": [0-9]+/"version": $VERSION_CODE/g' /dist/package.json
+#sed -i "s/\"version\": \".\..\..\"/\"version\": \"$VERSION\"/g" /dest/app.json
+#sed -i "s/\"version\": \".\..\..\"/\"version\": \"$VERSION\"/g" /dest/package.json
+
+jq ".version = \"$VERSION\"" /dest/app.json > /tmp/app.json && mv /tmp/app.json /dest/app.json
+jq ".versionCode = \"$VERSION_CODE\"" /dest/app.json > /tmp/app.json && mv /tmp/app.json /dest/app.json
+
+jq ".version = \"$VERSION\"" /dest/package.json > /tmp/package.json && mv /tmp/package.json /dest/package.json
+jq ".versionCode = \"$VERSION_CODE\"" /dest/package.json > /tmp/package.json && mv /tmp/package.json /dest/package.json
 
 echo "****** COPY FILES ******" >&2
 if [ -f "/source/.gitignore" ]; then
-  cp /source/.gitignore /dist/
+  cp /source/.gitignore /dest/
 fi
 
-cp -r /source/src /dist/
-cp /source/App.js /dist/
-cp /source/appSettings.template.json /dist
-cp /source/index.js /dist
+cp -r /source/src /dest/
+cp /source/App.js /dest/
+cp /source/appSettings.template.json /dest
+cp /source/index.js /dest
 
 echo "****** COMMIT & PUSH ******" >&2
-cd /dist
+cd /dest
 git add .
 git commit -am "action[repository-copy-action] $1 -> $2"
 git push
